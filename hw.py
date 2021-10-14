03b285af9c0f809d3e0d216bc63e7d251ff674e9
Piyash-max <71182127+Piyash-max@users.noreply.github.com>
2021-10-14 21:14:52 +0530

    Add files via upload

--- hw.py
+++ hw.py
@@ -1,13 +1,17 @@
 print("Number guessing game")
 print("Guess a number(between 5 and 9)")
 guess=3
-chances=5
+chances=3 
 number=(int(input("Enter your guess: ")))
-if (number<guess):
-    print("The number entered by u is low")
-elif (number>guess):
-    print("The number entered by u is high")
-elif (number==guess):
-    print("CONGO!! U HAVE GUESSED THE RIGHT NUMBER.")
-
-        
+while chances < 5:
+    if(number<guess):
+        print("The number entered by u is low")
+        number=(int(input("Enter your guess: ")))
+    elif(number>guess):
+        print("The number entered by u is high")
+        number=(int(input("Enter your guess: ")))
+    elif (number==guess):
+        print("CONGO!! U HAVE GUESSED THE RIGHT NUMBER.")
+        break
+if not chances < 5:
+    print("U lose!!")
